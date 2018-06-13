1d96fd2ca47a9a6fbd07ecdf8a1a940e5a438677
betty4229 <betty4229@gmail.com>
2018-06-13 22:46:35 +0800

    0613

---
+++ week2/js/script.js
@@ -0,0 +1,16 @@
+// $(function(){
+//     var navPos = $("nav").offset().top;
+
+//     $(window).scroll(function(){
+//         $(window).scrollTop();
+//     });
+// });
+
+// $(function(){
+//     var navPos = $(".nav").offset().top;
+
+//     if($(window).scrollTop() > navPos){
+//         $(".nav".css("position","fixed");
+//      }
+//     });
+// });
